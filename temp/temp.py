92e45f91d2139399e52e9162e3f609dfddf7d9fe
halsan23 <halsan23@msn.com>
2026-09-10 11:28:18 -0600

    temp changed working on practice coding badDoggy | 9/10/26

--- temp/temp.py
+++ temp/temp.py
@@ -1,18 +1,4 @@
-from random import randint
-x = randint(-100, 100)
-while x == 0:  # make sure x isn't zero
-    x = randint(-100, 100)
-y = randint(-100, 100)
-while y == 0:  # make sure y isn't zero
-    y = randint(-100, 100)
-
-
-
-if x > 0 and y > 0:
-     print(f"{x}, {y}: both positive")
-elif x < 0 and y < 0:
-    print(f"{x}, {y}: both negative")
-elif x > 0 and y < 0:
-    print(f"{x}, {y}: x is positive and y is negative")
-elif y > 0 and x < 0:
-    print(f"{x}, {y}: y is positive and x is negative")
+x = 0
+for num in range(11,20,2):
+   x += num
+print(x)
