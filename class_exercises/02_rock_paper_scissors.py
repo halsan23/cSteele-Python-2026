b61cddb30d9347f7c2889f77c6bda664a64c0d37
halsan23 <halsan23@msn.com>
2026-09-15 11:59:45 -0600

    working on updater Rock, Paper, Scissors game badDoggy | 9/15/26

--- class_exercises/02_rock_paper_scissors.py
+++ class_exercises/02_rock_paper_scissors.py
@@ -1,17 +1,16 @@
-##################################
+######################################
 # Rock - Paper - Scissors
-# Created by - badDoggy - | 09/26
-##################################
+# Created by - badDoggy - | 09/15/26
+######################################
 
 # Imports
 import subprocess
 import os
 from random import randint
 
-# Clear Screen
-subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
 
 # set variables
+computer_choices = ['rock', 'paper', 'scissors']
 user_score = 0
 comp_score = 0
 tie_score = 0
@@ -20,108 +19,114 @@ turn = 1
 
 # Random Computer selection
 rand_num = randint(0,2)
-if rand_num == 0:
-	comp = "rock"
-elif rand_num == 1:
-	comp = "paper"
-else:
-	comp = "scissors"
+comp = computer_choices[rand_num]
+
+
+# function to test for win
+def win_check(user, comp):
+   # if a tie
+   if user == comp:
+      winner = None
+      win_text = 'This round is a tie:'
+      return winner, win_text
+
+   # user win scenarios
+   elif user == 'rock' and comp == 'scissors':
+      winner = 'user'
+      win_text = 'You win this round: - Rock smashes Scissors'
+      return winner, win_text
+   elif user == 'paper' and comp == 'rock':
+      winner = 'user'
+      win_text = 'You win this round: - Paper covers Rock'
+      return winner, win_text
+   elif user == 'scissors' and comp == 'paper':
+      winner = 'user'
+      win_text = 'You win this round: - Scissors cut Paper'
+      return winner, win_text
+
+   # computer win scenarios
+   elif comp == 'rock' and user == 'scissors':
+      winner = 'computer'
+      win_text = 'Computer wins this round: - Rock smashes Scissors'
+      return winner, win_text
+   elif comp == 'paper' and user == 'rock':
+      winner = 'computer'
+      win_text = 'Computer wins this round: - Paper covers Rock'
+      return winner, win_text
+   else:
+      comp == 'scissors' and user == 'paper'
+      winner = 'computer'
+      win_text = 'Computer wins this round: - Scissors cut Paper'
+      return winner, win_text
+
 
 # Play the Game
-while user_score < win_score or comp_score < win_score:
+while user_score < win_score and comp_score < win_score:
+
+   # Clear Screen
+   subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
+
    # Title Bar
-   print(f'User Score: {user_score} | Computer Score: {comp_score}\n\n')
+   print(f'Winning Score: {win_score} | User Score: {user_score} | Computer Score: {comp_score} | Tie Games: {tie_score}\n\n')
    print('- Rock - Paper - Scissors -')
    print('- Player vs Computer -')
    print('- Best 3 out of 5 -\n')
    print(f'Round # {turn}\n')
+
    # User Input
-   # Computer choice display
-   user = input('Input Your Choice: ').lower()
+   user = input('Enter Your Choice: ').lower()
+
 
    # option for user to end game early
    if user.lower() == 'q' or user.lower() == 'quit':
       print('\n- User Quit -')
       break
+
+
    # invalid input check
    elif user.lower() != 'rock' and user.lower() != 'paper' and user.lower() != 'scissors':
-      print('-- Invalid Input --')
-      print("Please enter Rock, Paper, Scissors, or Quit!\n")
-      # input('\n- press any key to continue -')
-      # # Clear Screen for next round
-      # subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
+      print('\n- Invalid Input -\n')
+      if user == '':
+         print('- Empty User Input -')
+      print("- Enter (Q)uit or Rock, Paper, Scissors only! -\n")
+      # Pause play so reader can see the error info
+      input('- Press any key to continue -')
 
+   # Display computer pick
    else:
       print(f'Computer Choice: - {comp.capitalize()} -\n')
 
-      # if a tie
-      if user == comp:
-         print(f'This round is a tie:')
+
+      # No errors - Check for win
+      winner, win_text = win_check(user, comp)
+      print(win_text)
+
+
+      # Update Scores and round
+      if winner == 'user':
+         user_score += 1
+      elif winner == 'computer':
+         comp_score += 1
+      else:
          tie_score += 1
-         turn += 1
-         print(f'\nCurrent score: {user_score} to {comp_score}.')
-         input('\n- press any key to continue -')
-         # Clear Screen for next round
-         subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
-
-      # test for win and display results
-      elif user == 'rock' and comp == 'scissors':
-            print(f'You win this round: - Rock smashes Scissors')
-            user_score += 1
-            turn += 1
-            print(f'\nCurrent score: {user_score} to {comp_score}.')
-            input('\n- press any key to continue -')
-            # Clear Screen for next round
-            subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
-      elif user == 'paper' and comp == 'rock':
-            print(f'You win this round: - Paper covers Rock')
-            user_score += 1
-            turn += 1
-            print(f'\nCurrent score: {user_score} to {comp_score}.')
-            input('\n- press any key to continue -')
-            # Clear Screen for next round
-            subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
-      elif user == 'scissors' and comp == 'paper':
-            print(f'You win this round: - Scissors cut Paper')
-            user_score += 1
-            turn += 1
-            print(f'\nCurrent score: {user_score} to {comp_score}.')
-            input('\n- press any key to continue -')
-            # Clear Screen for next round
-            subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
-
-      elif comp == 'rock' and user == 'scissors':
-            print(f'Computer wins this round: - Rock smashes Scissors')
-            comp_score += 1
-            turn += 1
-            print(f'\nCurrent score: {user_score} to {comp_score}.')
-            input('\n- press any key to continue -')
-            # Clear Screen for next round
-            subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
-      elif comp == 'paper' and user == 'rock':
-            print(f'Computer wins this round: - Paper covers Rock')
-            comp_score += 1
-            turn += 1
-            print(f'\nCurrent score: {user_score} to {comp_score}.')
-            input('\n- press any key to continue -')
-            # Clear Screen for next round
-            subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
-      elif comp == 'scissors' and user == 'paper':
-            print(f'Computer wins this round: - Scissors cut Paper')
-            comp_score += 1
-            turn += 1
-            print(f'\nCurrent score: {user_score} to {comp_score}.')
-            input('\n- press any key to continue -')
-            # Clear Screen for next round
-            subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
+      turn += 1
+      # Pause play so reader can see the results of this round
+      input('\n- Press any key to continue -')
 
 
-print('- Game Over -')
-if user_score == comp_score:
-   print(f'\nGames ends in a TIE after {turn} rounds: {comp_score} to {user_score}.')
-elif comp_score > user_score:
-   print(f'\nComputer wins after {turn} rounds {comp_score} to {user_score}.')
+
+# Game Over
+# Clear Screen
+subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
+
+# Title Bar
+print(f'Winning Score: {win_score} | User Score: {user_score} | Computer Score: {comp_score} | Tie Games: {tie_score}\n\n')
+print('- Rock - Paper - Scissors -')
+print('- Player vs Computer -')
+print('- Best 3 out of 5 -\n')
+if user_score > comp_score:
+   print(f'\n\nYou Won, {user_score} games to {comp_score}, in {turn} rounds :)')
 else:
-   print(f'\nPlayer wins after {turn} rounds {user_score} to {comp_score}.')
+   print(f'\n\nThe computer wins, {comp_score} games to {user_score}, in {turn} rounds :(')
 print('\nThanks for playing ...')
 print('\n\n-- End of Line --\n')
--- temp/temp.py
+++ temp/temp.py
@@ -1,23 +1,127 @@
-###################################################
-# temp practice exercises for csteele Python class
-###################################################
+##################################
+# Rock - Paper - Scissors
+# Created by - badDoggy - | 09/26
+##################################
 
+# Imports
+import subprocess
 import os
-os.system('cls')
+from random import randint
 
-from random import randint  # use randint(a, b) to generate a random number between a and b
+# Clear Screen
+subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
 
-number = randint(1,10)
-i = 0  # i should be incremented by one each iteration
+# set variables
+user_score = 0
+comp_score = 0
+tie_score = 0
+win_score = 3
+turn = 1
 
-while number != 5:
-   i += 1
-   # print(f'Run number is: {i}')
-   # print(f'Random Number is: {number}\n')
-   number = randint(1,10)
+# Random Computer selection
+rand_num = randint(0,2)
+if rand_num == 0:
+	comp = "rock"
+elif rand_num == 1:
+	comp = "paper"
+else:
+	comp = "scissors"
 
-print('Random number generation between 1 and 10.\n')
-print(f'took {i + 1} runs to generate a random number of 5.')
+# Play the Game
+while user_score < win_score or comp_score < win_score:
+   # Title Bar
+   print(f'User Score: {user_score} | Computer Score: {comp_score}\n\n')
+   print('- Rock - Paper - Scissors -')
+   print('- Player vs Computer -')
+   print('- Best 3 out of 5 -\n')
+   print(f'Round # {turn}\n')
+   # User Input
+   # Computer choice display
+   user = input('Input Your Choice: ').lower()
+
+   # option for user to end game early
+   if user.lower() == 'q' or user.lower() == 'quit':
+      print('\n- User Quit -')
+      break
+   # invalid input check
+   elif user.lower() != 'rock' and user.lower() != 'paper' and user.lower() != 'scissors':
+      print('-- Invalid Input --')
+      print("Please enter Rock, Paper, Scissors, or Quit!\n")
+      # input('\n- press any key to continue -')
+      # # Clear Screen for next round
+      # subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
+
+   else:
+      print(f'Computer Choice: - {comp.capitalize()} -\n')
+
+      # if a tie
+      if user == comp:
+         print(f'This round is a tie:')
+         tie_score += 1
+         turn += 1
+         print(f'\nCurrent score: {user_score} to {comp_score}.')
+         input('\n- press any key to continue -')
+         # Clear Screen for next round
+         subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
+
+      # test for win and display results
+      elif user == 'rock' and comp == 'scissors':
+            print(f'You win this round: - Rock smashes Scissors')
+            user_score += 1
+            turn += 1
+            print(f'\nCurrent score: {user_score} to {comp_score}.')
+            input('\n- press any key to continue -')
+            # Clear Screen for next round
+            subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
+      elif user == 'paper' and comp == 'rock':
+            print(f'You win this round: - Paper covers Rock')
+            user_score += 1
+            turn += 1
+            print(f'\nCurrent score: {user_score} to {comp_score}.')
+            input('\n- press any key to continue -')
+            # Clear Screen for next round
+            subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
+      elif user == 'scissors' and comp == 'paper':
+            print(f'You win this round: - Scissors cut Paper')
+            user_score += 1
+            turn += 1
+            print(f'\nCurrent score: {user_score} to {comp_score}.')
+            input('\n- press any key to continue -')
+            # Clear Screen for next round
+            subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
+
+      elif comp == 'rock' and user == 'scissors':
+            print(f'Computer wins this round: - Rock smashes Scissors')
+            comp_score += 1
+            turn += 1
+            print(f'\nCurrent score: {user_score} to {comp_score}.')
+            input('\n- press any key to continue -')
+            # Clear Screen for next round
+            subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
+      elif comp == 'paper' and user == 'rock':
+            print(f'Computer wins this round: - Paper covers Rock')
+            comp_score += 1
+            turn += 1
+            print(f'\nCurrent score: {user_score} to {comp_score}.')
+            input('\n- press any key to continue -')
+            # Clear Screen for next round
+            subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
+      elif comp == 'scissors' and user == 'paper':
+            print(f'Computer wins this round: - Scissors cut Paper')
+            comp_score += 1
+            turn += 1
+            print(f'\nCurrent score: {user_score} to {comp_score}.')
+            input('\n- press any key to continue -')
+            # Clear Screen for next round
+            subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
 
 
+print('- Game Over -')
+if user_score == comp_score:
+   print(f'\nGames ends in a TIE after {turn} rounds: {comp_score} to {user_score}.')
+elif comp_score > user_score:
+   print(f'\nComputer wins after {turn} rounds {comp_score} to {user_score}.')
+else:
+   print(f'\nPlayer wins after {turn} rounds {user_score} to {comp_score}.')
+print('\nThanks for playing ...')
 print('\n\n-- End of Line --\n')
