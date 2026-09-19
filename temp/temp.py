86aa856e47eaad298f271caece926ac93d4e3c8b
halsan23 <halsan23@msn.com>
2026-09-19 11:44:35 -0600

    started a text file in the misc folder defining different list methods badDoggy | 9/19/26

--- temp/temp.py
+++ temp/temp.py
@@ -4,13 +4,26 @@ import os
 subprocess.run("cls" if os.name == "nt" else "clear", shell=True)
 
 
-
-
-# Define your code below:
+# Create a list called instructors
 instructors = []
-instructors.extend(['Colt', 'Blue', 'Lisa'])
-for name in instructors:
-    print(name)
 
-instructors.clear()
+# Add the following strings to the instructors list
+    # "Colt"
+    # "Blue"
+    # "Lisa"
+instructors.extend(['Colt', 'Blue','Lisa'])
+print(instructors)
+
+# Remove the last value in the list
+instructors.pop()
+print(instructors)
+
+# Remove the first value in the list
+instructors.pop(0)
+print(instructors)
+
+# Add the string "Done" to the beginning of the list
+instructors.insert(0, 'Done')
+
+# Run the tests to make sure you've done this correctly!
 print(instructors)
