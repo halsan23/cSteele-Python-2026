20f8104f8dbafc8f2d1d8365595bfb10f971409f
badDoggy <halsan23@msn.com>
2026-09-24 17:25:06 -0600

    started learning python3 dictionary'started modified temp.py practicing badDoggy

--- temp/temp.py
+++ temp/temp.py
@@ -3,3 +3,19 @@ import subprocess
 import os
 
 subprocess.run("cls" if os.name == "nt" else "clear", shell=True)
+
+
+# DON'T TOUCH PLEASE!
+donations = dict(
+    sam=25.0, lena=88.99, chuck=13.0, linus=99.5, stan=150.0, lisa=50.25, harrison=10.0
+)
+# DON'T TOUCH PLEASE!
+
+
+# Use a loop to add together all the donations and store the resulting number in a variable called total_donations
+total_donations = 0
+
+for val in donations.values():
+    total_donations += val
+
+print(total_donations)
